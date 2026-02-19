53c2b225fa795ade0cb76d8f8b83954a222553fc
chetat <yekuwilfred@gmail.com>
2026-02-19 19:26:30 +0400

    add config-driven augmentation pipeline with bbox support

---
+++ visionpipe/data/augmentations.py
@@ -0,0 +1,27 @@
+import albumentations as A
+from omegaconf import DictConfig
+
+
+def build_transforms(cfg: DictConfig, is_train: bool) -> A.Compose:
+    bbox_params = A.BboxParams(
+        format="pascal_voc",
+        label_fields=["labels"],
+        min_visibility=0.3,
+    )
+
+    aug_cfg = cfg.augmentation
+
+    if not aug_cfg.enabled or not is_train:
+        return A.Compose([], bbox_params=bbox_params)
+
+    transforms = [
+        A.RandomBrightnessContrast(
+            brightness_limit=aug_cfg.brightness_limit,
+            contrast_limit=aug_cfg.contrast_limit,
+            p=0.5,
+        ),
+        A.Rotate(limit=aug_cfg.rotation_limit, p=0.5),
+        A.HorizontalFlip(p=aug_cfg.horizontal_flip_prob),
+    ]
+
+    return A.Compose(transforms, bbox_params=bbox_params)
